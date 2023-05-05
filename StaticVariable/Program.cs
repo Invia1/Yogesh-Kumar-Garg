35926958836bd99018d8620d3bf17d9d7ac9c5ca
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 13:55:09 +0530

    Add files via upload

---
+++ StaticVariable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVariable
+{
+    class employee
+    {
+        public string empName;
+        public static string organizationName;
+        public  int employeeId;
+
+         static employee()
+        {
+            organizationName= "invia";   // initialize static variable in static constructor
+        }
+        static void Main(string[] args)
+        {
+            employee emp1= new employee();
+            emp1.empName = "yug";
+            organizationName = "invia private limited"; // changable 
+            emp1.employeeId = 4218;
+
+            employee emp2= new employee();
+            emp2.empName = "lalit";
+            emp2.employeeId = 4219;
+
+            
+            Console.WriteLine("employee name---{0}, employee id----{1}, organization name----{2}",emp1.empName,emp1.employeeId,organizationName);
+            Console.WriteLine("employee name---{0}, employee id----{1}, organization name----{2}", emp2.empName, emp2.employeeId, organizationName);
+              Console.ReadLine();
+        }
+
+    }
+}
